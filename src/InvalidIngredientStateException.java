e7091983b0ca307ceac02e2a9e958c2cc6ed4daa
Nadine <18224012@std.stei.itb.ac.id>
2025-12-08 20:42:50 +0700

    feat: add InvalidIngredientStateException class for handling invalid ingredient states

---
+++ src/InvalidIngredientStateException.java
@@ -0,0 +1,10 @@
+package src;
+
+
+public class InvalidIngredientStateException extends ItemException { // kelas exception untuk  state ingredient yang salah
+
+    public InvalidIngredientStateException(String message) {
+        super(message);
+    }
+    
+}
